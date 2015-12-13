0f2880fdc99c713624805122bb8a24ea9a961c46
Boubacar Diarra <boubadiarra@gmail.com>
2015-12-13 19:48:24 +0100

    bootstrap fix

--- public/js/views/main.ts
+++ public/js/views/main.ts
@@ -6,10 +6,7 @@ export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-		.globalResources(['js/views/resources/nav-bar', 'js/views/resources/connect-bar', 'js/views/resources/person-bar',
-			'js/views/resources/sheader-bar', 'js/views/resources/work-bar', 'js/views/resources/pagination-bar', 'js/views/resources/avatar-bar',
-			'js/views/resources/command-bar', 'js/views/resources/elements-bar', 'js/views/resources/siglename-bar',
-			'js/views/resources/interval-bar','js/views/resources/depart-bar','js/views/resources/depannee-bar',
-			'js/views/resources/depunite-bar','js/views/resources/import-bar']);
+	.plugin('aurelia-animator-css')
+	.feature('js/views/resources');
   aurelia.start().then(a => a.setRoot('js/views/app'));
 }
