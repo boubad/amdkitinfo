7a8dae6ec69ab78a66fa91c7a766bc9c0caa8dee
Boubacar Diarra <boubadiarra@gmail.com>
2015-12-15 21:33:08 +0100

    remove debug config

--- public/js/views/main.ts
+++ public/js/views/main.ts
@@ -5,7 +5,6 @@ import {Aurelia} from 'aurelia-framework';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging()
-		.feature('js/views/resources');
+	.feature('js/views/resources');
   aurelia.start().then(a => a.setRoot('js/views/app'));
 }
